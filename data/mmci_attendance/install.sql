e34a76e39ea94969f9ac5439e9ce828e97a63e37
gabbydgab <gamba@gabbydgab.com>
2014-11-21 15:53:59 +0800

    updated table schema

--- data/mmci_attendance/install.sql
+++ data/mmci_attendance/install.sql
@@ -2,7 +2,26 @@
 CREATE DATABASE IF NOT EXISTS `mmci_attendance`;
 USE `mmci_attendance`;
 
-CREATE TABLE IF NOT EXISTS `work_schedule` () ENGINE=InnoDB CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `work_schedule` (
+    `work_schedule_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `hpw` TINYINT(4) UNSIGNED NOT NULL DEFAULT 0,
+    `mon` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
+    `tue` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
+    `wed` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
+    `thu` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
+    `fri` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
+    `sat` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '0',
+    `sun` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '0'
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `employee_work_schedule` () ENGINE=InnoDB CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `employee_work_schedule` (
+    `ews_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `employee_id` VARCHAR(100) NOT NULL DEFAULT "",
+    `work_schedule_id` INT(10) UNSIGNED NOT NULL DEFAULT 1,
+) ENGINE=InnoDB CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `break_schedule` () ENGINE=InnoDB CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `employee_break_schedule` () ENGINE=InnoDB CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `timeclock` () ENGINE=InnoDB CHARSET=utf8;
