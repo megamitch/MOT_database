95017e401199c0927b97620be0a932081a471bae
gabbydgab <gamba@gabbydgab.com>
2014-12-04 15:34:29 +0800

    updated table schema

--- data/mmci_attendance/install.sql
+++ data/mmci_attendance/install.sql
@@ -2,39 +2,17 @@
 CREATE DATABASE IF NOT EXISTS `mmci_attendance`;
 USE `mmci_attendance`;
 
-CREATE TABLE IF NOT EXISTS `work_schedule` (
-    `work_schedule_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `hpw` TINYINT(4) UNSIGNED NOT NULL DEFAULT 0,
-    `mon` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
-    `tue` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
-    `wed` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
-    `thu` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
-    `fri` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
-    `sat` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '0',
-    `sun` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '0'
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `employee_work_schedule` (
-    `ews_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `employee_time_log` (
+    `time_log_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `employee_id` VARCHAR(100) NOT NULL DEFAULT '',
-    `work_schedule_id` INT(10) UNSIGNED NOT NULL DEFAULT 1,
-    `timeclock_schedule_id` INT(10) UNSIGNED NOT NULL DEFAULT 1,
-    INDEX emp_work_sched (`employee_id`,`work_schedule_id`, `timeclock_schedule_id`),
-    INDEX emp_id (`employee_id`),
-    UNIQUE employee_id (`employee_id`),
-    UNIQUE work_schedule (`employee_id`, `work_schedule_id`),
+    `log_date` DATE NOT NULL DEFAULT '0000-00-00',
+    INDEX employee (`employee_id`),
+    INDEX logdate (`log_date`),
+    INDEX employeelog (`employee_id`, `log_date`),
+    UNIQUE employee_log_date (`employee_id`, `log_date`),
     FOREIGN KEY (`employee_id`) 
         REFERENCES `mmci_main`.`employee` (`employee_id`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `timeclock_schedule` (
-    `timeclock_schedule_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `clock_in` TIME NOT NULL DEFAULT "00:00:00",
-    `clock_out` TIME NOT NULL DEFAULT "00:00:00",
-    `total_hours` TINYINT(2) NOT NULL DEFAULT 0,
-    INDEX time_in_out (`clock_in`, `clock_out`),
-    UNIQUE timeclock (`clock_in`, `clock_out`)
-) ENGINE=InnoDB;
 
 -- CREATE TABLE IF NOT EXISTS `break_schedule` () ENGINE=InnoDB CHARSET=utf8;
 
@@ -47,9 +25,7 @@ CREATE TABLE IF NOT EXISTS `timeclock` (
     `log_type` ENUM ('time-in', 'time-out') NOT NULL,
     `log_time` TIME NOT NULL DEFAULT '00:00:00',
     `log_date` DATE NOT NULL DEFAULT '0000-00-00',
-    UNIQUE employee_record (`employee_id`, `log_date`, `log_type`, `branch_code`),
-    FOREIGN KEY (`employee_id`) 
-        REFERENCES `mmci_main`.`employee` (`employee_id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    UNIQUE employee_record (`employee_id`, `log_date`, `log_type`, `branch_code`),    
     FOREIGN KEY (`branch_code`)
         REFERENCES `mmci_main`.`branch` (`branch_code`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB CHARSET=utf8;
--- data/mmci_main/install.sql
+++ data/mmci_main/install.sql
@@ -59,6 +59,42 @@ CREATE TABLE IF NOT EXISTS `employee` (
 ) ENGINE=InnoDB CHARSET=utf8;
 
 
+CREATE TABLE IF NOT EXISTS `work_schedule` (
+    `work_schedule_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `hpw` TINYINT(4) UNSIGNED NOT NULL DEFAULT 0,
+    `mon` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
+    `tue` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
+    `wed` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
+    `thu` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
+    `fri` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '1',
+    `sat` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '0',
+    `sun` ENUM ('0', '1', '2', '3') NOT NULL DEFAULT '0'
+) ENGINE=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `employee_work_schedule` (
+    `ews_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `employee_id` VARCHAR(100) NOT NULL DEFAULT '',
+    `work_schedule_id` INT(10) UNSIGNED NOT NULL DEFAULT 1,
+    `timeclock_schedule_id` INT(10) UNSIGNED NOT NULL DEFAULT 1,
+    INDEX emp_work_sched (`employee_id`,`work_schedule_id`, `timeclock_schedule_id`),
+    INDEX emp_id (`employee_id`),
+    UNIQUE employee_id (`employee_id`),
+    UNIQUE work_schedule (`employee_id`, `work_schedule_id`),
+    FOREIGN KEY (`employee_id`) 
+        REFERENCES `employee` (`employee_id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `timeclock_schedule` (
+    `timeclock_schedule_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `clock_in` TIME NOT NULL DEFAULT "00:00:00",
+    `clock_out` TIME NOT NULL DEFAULT "00:00:00",
+    `total_hours` TINYINT(2) NOT NULL DEFAULT 0,
+    INDEX time_in_out (`clock_in`, `clock_out`),
+    UNIQUE timeclock (`clock_in`, `clock_out`)
+) ENGINE=InnoDB;
+
+
 CREATE TABLE IF NOT EXISTS `employee_leave_credits` (
     `elc_id` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `employee_id` VARCHAR(100) NOT NULL DEFAULT '',
